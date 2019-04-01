7932be59b59a3fe7c3024fdeceaeba849886c693
Hiba <hibaelji@outlook.com>
2019-04-01 15:21:29 +0200

    TAPJES TOEVOEGEN BIJ MEME API JS

--- scripts/meme.js
+++ scripts/meme.js
@@ -1,14 +1,14 @@
 //window onload
 window.onload = function() {
-//make variable with the link to the api
-var api = "https://api.giphy.com/v1/gifs/trending?";
-//make variable with the api key
-var apiKey = "&api_key=Wcg2bvD303bHYF95u2LhwgTRbHr0i5K2";
-//make variable with the query -> limit is 3 gifs
-var query = "&limit=3&rating=G";
+	//make variable with the link to the api
+	var api = "https://api.giphy.com/v1/gifs/trending?";
+	//make variable with the api key
+	var apiKey = "&api_key=Wcg2bvD303bHYF95u2LhwgTRbHr0i5K2";
+	//make variable with the query -> limit is 3 gifs
+	var query = "&limit=3&rating=G";
 
-//function to get the API data
-function getAPIdata() {
+	//function to get the API data
+	function getAPIdata() {
 	//create url with the variables made
 	var url = api + apiKey + query;
 	//fetch
@@ -18,15 +18,15 @@ function getAPIdata() {
 		return response.json();
 	})
 
-	.then(function(response) {
-		//for loopie
-		for (var i = 0; i < response.data.length; i++){
-			//console.log(response.data[i].images);
-		//Put an image tag in the div with id "gifbox" and show the response in it (aka the gif)
-			document.getElementById("gifbox").innerHTML += '<img class="gifs" src=" ' + response.data[i].images.fixed_width.url + '">' + '<br>';
-		}
-	});
-}
+		.then(function(response) {
+			//for loopie
+			for (var i = 0; i < response.data.length; i++){
+				//console.log(response.data[i].images);
+			//Put an image tag in the div with id "gifbox" and show the response in it (aka the gif)
+				document.getElementById("gifbox").innerHTML += '<img class="gifs" src=" ' + response.data[i].images.fixed_width.url + '">' + '<br>';
+			}
+		});
+	}
 
-getAPIdata();
+	getAPIdata();
 }
